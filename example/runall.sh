a8cf70705cf5f8a1bd3054547da3de2bae9aebbc
Jörg Ramb <jorg@jramb.com>
2013-03-31 22:42:52 +0200

    renamed templater.clj

--- example/runall.sh
+++ example/runall.sh
@@ -1,4 +1,4 @@
 lein run example/test.xls  example/out-tabsep1.txt  example/tabsep.clj
 lein run example/test.xlsx example/out-tabsep2.txt  example/tabsep.clj
 lein run example/test.xlsx example/out-emacstab.txt example/emacstab.clj
-lein run example/data.xlsx example/out-sql-insert.sql example/sql-insert.clj example/sql-template.sql
+lein run example/data.xlsx example/out-sql-insert.sql example/templater.clj example/sql-template.sql
